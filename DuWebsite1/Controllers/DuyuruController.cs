4ca20c99be4a18cccf290dc30264c8062f9f738a
Halil Ateş <67061715+HalilAtes@users.noreply.github.com>
2022-12-02 16:07:09 +0300

    Add project files.

---
+++ DuWebsite1/Controllers/DuyuruController.cs
@@ -0,0 +1,36 @@
+﻿using DuWebsite1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DuWebsite1.Controllers
+{
+    public class DuyuruController : Controller
+    {
+        EFDuWebsiteRepository websiteRepository = new EFDuWebsiteRepository(new DuDbContext());
+        public int PageSize = 10;
+
+    
+        public ActionResult Index()
+        {
+            var Duyurular = websiteRepository.Duyurular;
+
+            return View(Duyurular);
+        }
+
+        public ActionResult DuyuruDetay(int id)
+        {
+            var duyuru = websiteRepository.Duyurular.FirstOrDefault(x => x.DuyuruId == id);
+            return View(duyuru);
+        }
+
+        public ActionResult Privacy()
+        {
+            return View();
+        }
+
+
+    }
+}
