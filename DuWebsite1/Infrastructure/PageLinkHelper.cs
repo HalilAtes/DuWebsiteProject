56efbfb869d762e86b898053099a5428cfef1e07
Halil Ateş <67061715+HalilAtes@users.noreply.github.com>
2022-12-05 23:17:30 +0300

    Pagging Eklenti - Duyuru Hover(önemli :))

--- DuWebsite1/Infrastructure/PageLinkHelper.cs
+++ DuWebsite1/Infrastructure/PageLinkHelper.cs
@@ -24,6 +24,18 @@ namespace DuWebsite1.Infrastructure
          public ViewContext ViewContext { get; set; }
          public PagingInfo PageModel { get; set; }
         public string PageAction { get; set; }
+
+        [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
+        public Dictionary<string, object> PageUrlValues { get; set; }
+        = new Dictionary<string, object>();
+
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+
+        public string PageClassNormal { get; set; }
+
+        public string PageClassSelected { get; set; }
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
